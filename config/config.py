20b4523d9697160a8de1c3a0136c8019eff03b42
ItsDrike <itsdrikeofficial@gmail.com>
2020-11-30 03:25:41 +0100

    Add basic package config

---
+++ config/config.py
@@ -0,0 +1,7 @@
+# Package information
+NAME = "securepy"
+VERSION = "v0.0.1 - alpha"
+AUTHOR = "ItsDrike"
+AUTHOR_MAIL = "itsdrikeofficial@gmail.com"
+DESCRIPTION = "Secure unknown python code execution"
+LICENCE = "Apache-2.0"
