5630f5f38dc781b370b9b9c942f3b591c5c95127
ItsDrike <itsdrikeofficial@gmail.com>
2020-11-30 13:28:02 +0100

    Add dunder package info

---
+++ securepy/__init__.py
@@ -0,0 +1,8 @@
+import config.config as _conf
+from securepy.sandbox import Sandbox  # noqa
+
+__title__ = _conf.NAME
+__author__ = _conf.AUTHOR
+__licence__ = _conf.LICENCE
+__copyright__ = _conf.COPYRIGHT
+__version__ = _conf.VERSION
