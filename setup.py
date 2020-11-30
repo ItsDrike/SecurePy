d5c09a1ffb12a7581ceb76b654dafe4a18b8616f
ItsDrike <itsdrikeofficial@gmail.com>
2020-11-30 03:33:06 +0100

    Add basic package setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+from config import config
+
+DIR = Path(__file__).parent
+README = (DIR / "README.md").read_text()
+
+setup(
+    name=config.NAME,
+    version=config.VERSION,
+    author=config.AUTHOR,
+    author_email=config.AUTHOR_MAIL,
+    description=config.DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license=config.LICENCE,
+    url="https://github.com/ItsDrike/SecurePy",
+    classifiers=[
+        f"License :: OSI Approved :: {config.LICENCE} License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Linux",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=[],
+    python_requires='>=3.7',
+)
+
