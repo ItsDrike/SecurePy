d5110adb7a32343788997a0cc4f60a36cd96e4d3
ItsDrike <itsdrikeofficial@gmail.com>
2020-12-01 17:21:08 +0100

    Raise runtime warning if function has return value

--- securepy/timer.py
+++ securepy/timer.py
@@ -7,10 +7,25 @@ class TimeLimiter():
     def __init__(self, time_limit: int):
         self.time_limit = time_limit
 
+    def _capture_return(self, func: t.Callable) -> t.Callable:
+        @wraps(func)
+        def inner(*args, **kwargs):
+            # Check if function returned some value, if it did
+            # raise a runtime warning because multiprocessing can't
+            # handle transferring values between processes (apart from ctypes)
+            if ret := func(*args, **kwargs):
+                raise RuntimeWarning(
+                    f"Function `{func.__name__}` attempted to return a value: `{ret}`, "
+                    "but value return isn't supported with TimeLimiter as it's not possible "
+                    "to share values between processes."
+                )
+        return inner
+
     def __call__(self, func: t.Callable) -> t.Callable:
         @wraps(func)
         def inner(*args, **kwargs) -> None:
-            timer = Process(target=func, args=args, kwargs=kwargs)
+            wrapped = self._capture_return(func)
+            timer = Process(target=wrapped, args=args, kwargs=kwargs)
             timer.start()
             # Wait for `self.time_limit` and join
             timer.join(self.time_limit)
