a8218deac569eb9bdf790f4799660f0f0c52201f
ItsDrike <itsdrikeofficial@gmail.com>
2020-12-02 19:10:44 +0100

    Remove old timer functionality

--- securepy/timer.py
+++ securepy/timer.py
@@ -1,7 +1,6 @@
 import multiprocessing
 import multiprocessing.pool
 import typing as t
-from functools import wraps
 
 
 class NoDaemonProcess(multiprocessing.Process):
@@ -61,7 +60,7 @@ def timed_run(time_limit: int, func: t.Callable, args=None, kwargs=None) -> t.An
 
     (NOTICE: This is NOT a decorator, only a wrapper, decorator functionality
     can't be added, because multiprocessing uses `pickle` internally which
-    tries to import given function (`foo`) which wouldn't be the same function
+    tries to import given function (`func`) which wouldn't be the same function
     as the passed one since it would be altered by the decorator itself, causing
     `PicklingError`)
     """
@@ -85,66 +84,3 @@ def timed_run(time_limit: int, func: t.Callable, args=None, kwargs=None) -> t.An
     pool.join()
 
     return value
-
-
-class TimedFunction():
-    """
-    This class is used as a decorator to limit function
-    execution of the decorated function by specified amount
-    of seconds.
-
-    If a function takes more time to execute than the specified
-    limit, it will be terminated and `TimeoutError` will be raised.
-
-    NOTICE: In order to provide a decorator functionality like this
-    any passed function can't have any return value because multiprocessing
-    has certain limitations for sharing values between processes.
-    (In case a return value is present, `RuntimeWarning` will be raised
-    and the return value will change to `None`).
-
-    If you need to access the return value, you should use `timed_run` wrapper
-    function instead, it works only as a wrapper, not a full decorator, but
-    because of this it's able to extract the returned value.
-    """
-
-    def __init__(self, time_limit: int):
-        self.time_limit = time_limit
-
-    def _capture_return(self, func: t.Callable) -> t.Callable:
-        """
-        Decorate given function and capture it's return value,
-        if this value isn't `None`, raise a `RuntimeWarning` and
-        return `None`.
-
-        This is necessary, because  multiprocessing can't handle
-        transferring values between processes (apart from ctypes).
-        """
-        @wraps(func)
-        def inner(*args, **kwargs):
-            if ret := func(*args, **kwargs) is not None:
-                raise RuntimeWarning(
-                    f"Function `{func.__name__}` attempted to return a value: `{ret}`, "
-                    "but value return isn't supported with TimeLimiter as it's not possible "
-                    "to share values between processes."
-                )
-        return inner
-
-    def __call__(self, func: t.Callable) -> t.Callable:
-        """
-        Decorate given function and run it concurrently with a timer,
-        using multiprocessing. If the timer reaches specified time limit,
-        and the function didn't end, raise `TimeoutError`, otherwise return `None`.
-        """
-        @wraps(func)
-        def inner(*args, **kwargs) -> None:
-            wrapped = self._capture_return(func)
-            proc = multiprocessing.Process(target=wrapped, args=args, kwargs=kwargs)
-            proc.start()
-            # Wait for `self.time_limit` and join
-            # The joining will happen sooner, in case the process ends before the time limit
-            proc.join(self.time_limit)
-
-            if proc.is_alive():
-                proc.terminate()
-                raise TimeoutError(f"Function `{func.__name__}` took longer than the allowed time limit ({self.time_limit})")
-        return inner
