b5732bc24f1d95bec6170d9a3f59285e3c89309a
ItsDrike <itsdrikeofficial@gmail.com>
2020-12-02 14:02:22 +0100

    Add wrapper with return capabilities

--- securepy/timer.py
+++ securepy/timer.py
@@ -1,9 +1,93 @@
+import multiprocessing
+import multiprocessing.pool
 import typing as t
 from functools import wraps
-from multiprocessing import Process
 
 
-class TimeLimiter():
+class NoDaemonProcess(multiprocessing.Process):
+    """
+    This class overrides `multiprocessing.Process` in order to make sure
+    that `daemon` property will stay False, this is necessary, because
+    daemonic processes aren't allowed to have children by multiprocessing.
+    """
+    @property
+    def daemon(self):
+        return False
+
+    @daemon.setter
+    def daemon(self, value):
+        pass
+
+
+# We obtain the type from `get_context` function because it's different
+# depending on the operating system used
+class NoDaemonContext(type(multiprocessing.get_context())):
+    """
+    This makes sure we use non-daemonic process for given
+    context (based on running OS), this is necessary, because
+    daemonic processes aren't allowed to have children by multiprocessing.
+    """
+    Process = NoDaemonProcess
+
+
+# We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
+# because the latter is only a wrapper function, not a proper class.
+class NestablePool(multiprocessing.pool.Pool):
+    """
+    Ensure that all processes within this pool will always stay non-daemonic,
+    this is done because daemonic processes aren't allowed to have
+    children by multiprocessing.
+    """
+    def __init__(self, *args, **kwargs):
+        kwargs['context'] = NoDaemonContext()  # noqa
+        super(NestablePool, self).__init__(*args, **kwargs)
+
+
+def timed_run(time_limit: int, func: t.Callable, args=None, kwargs=None) -> t.Any:
+    """
+    This function is used as a wrapper for given function `func`, it
+    enforces given `time_limit` (in seconds) on the execution of
+    this given function.
+
+    If a function takes more time to execute than the specified
+    limit, it will be terminated and `TimeoutError` will be raised.
+    Otherwise, it's return value will be returned
+
+    It works by running this function concurrently with a timer, using
+    multiprocessing, we than wait for given amount of seconds for the function
+    to end and return it's value, if this is before the time limit is reached,
+    this value will be returned and the branched process will be joined back,
+    otherwise, the function process will be terminated and the error will be raised.
+
+    (NOTICE: This is NOT a decorator, only a wrapper, decorator functionality
+    can't be added, because multiprocessing uses `pickle` internally which
+    tries to import given function (`foo`) which wouldn't be the same function
+    as the passed one since it would be altered by the decorator itself, causing
+    `PicklingError`)
+    """
+    if args is None:
+        args = []
+    if kwargs is None:
+        kwargs = {}
+
+    pool = NestablePool(processes=1)
+    result = pool.apply_async(func, args=args, kwds=kwargs)
+
+    try:
+        value = result.get(time_limit)
+    except multiprocessing.TimeoutError:
+        pool.terminate()
+        pool.close()
+        pool.join()
+        raise TimeoutError(f"Function took longer than the allowed time limit ({time_limit})")
+
+    pool.close()
+    pool.join()
+
+    return value
+
+
+class TimedFunction():
     """
     This class is used as a decorator to limit function
     execution of the decorated function by specified amount
@@ -12,11 +96,17 @@ class TimeLimiter():
     If a function takes more time to execute than the specified
     limit, it will be terminated and `TimeoutError` will be raised.
 
-    Note that any passed function can't have any return value due
-    to multiprocessing limitation of sharing values between processes.
+    NOTICE: In order to provide a decorator functionality like this
+    any passed function can't have any return value because multiprocessing
+    has certain limitations for sharing values between processes.
     (In case a return value is present, `RuntimeWarning` will be raised
     and the return value will change to `None`).
+
+    If you need to access the return value, you should use `timed_run` wrapper
+    function instead, it works only as a wrapper, not a full decorator, but
+    because of this it's able to extract the returned value.
     """
+
     def __init__(self, time_limit: int):
         self.time_limit = time_limit
 
@@ -48,12 +138,13 @@ class TimeLimiter():
         @wraps(func)
         def inner(*args, **kwargs) -> None:
             wrapped = self._capture_return(func)
-            timer = Process(target=wrapped, args=args, kwargs=kwargs)
-            timer.start()
+            proc = multiprocessing.Process(target=wrapped, args=args, kwargs=kwargs)
+            proc.start()
             # Wait for `self.time_limit` and join
-            timer.join(self.time_limit)
+            # The joining will happen sooner, in case the process ends before the time limit
+            proc.join(self.time_limit)
 
-            if timer.is_alive():
-                timer.terminate()
+            if proc.is_alive():
+                proc.terminate()
                 raise TimeoutError(f"Function `{func.__name__}` took longer than the allowed time limit ({self.time_limit})")
         return inner
