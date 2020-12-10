180991855e8a0b3a3bb84442298c491a8c6dd7be
ItsDrike <itsdrikeofficial@gmail.com>
2020-12-10 15:10:18 +0100

    Add main file to be run as subprocess

---
+++ securepy/executor.py
@@ -0,0 +1,25 @@
+import sys
+
+from security import get_globals
+
+"""
+This file will be run in subprocess and it's where the true code
+execution is happening.
+
+You need to provide system arguments to define,
+the code that will be executed and the restriction level
+
+Example usage:
+    `python securepy/executor.py [restriction_level] [code]`
+"""
+
+if __name__ == "__main__":
+    try:
+        restriction_level = int(sys.argv[1])
+        code = sys.argv[2]
+    except IndexError:
+        raise RuntimeError("Warning, some arguments are missing.")
+    except ValueError:
+        raise RuntimeError("Warning, some arguments aren't correct.")
+
+    exec(code, get_globals(restriction_level), {})
