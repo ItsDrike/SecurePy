71d7a4c0d43034ac35c7f2c7788b9e13c84b88ca
ItsDrike <itsdrikeofficial@gmail.com>
2020-12-20 23:46:12 +0100

    Fix import error (class renamed)

--- securepy/__init__.py
+++ securepy/__init__.py
@@ -2,7 +2,7 @@ import securepy.config.config as _conf
 from securepy.restrictor import Restrictor  # noqa
 from securepy.security import RESTRICTED_GLOBALS, SAFE_GLOBALS, UNRESTRICTED_GLOBALS  # noqa
 from securepy.stdio import IOCage, MemoryOverflow, read_process_output, LimitedStringIO  # noqa
-from securepy.timing import CapturingTimedFunction, TimedFunction, TimedFunctionError  # noqa
+from securepy.timing import IOTimedFunction, TimedFunction, TimedFunctionError  # noqa
 
 __title__ = _conf.NAME
 __author__ = _conf.AUTHOR
