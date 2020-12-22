416858af5e3fa8943e4744af1e54fb8dd43e1754
ItsDrike <itsdrikeofficial@gmail.com>
2020-12-22 19:37:00 +0100

    Add LimitedProcess to __init__

--- securepy/__init__.py
+++ securepy/__init__.py
@@ -1,7 +1,8 @@
 import securepy.config.config as _conf
+from securepy.limited_process import LimitedProcess  # noqa
 from securepy.restrictor import Restrictor  # noqa
 from securepy.security import RESTRICTED_GLOBALS, SAFE_GLOBALS, UNRESTRICTED_GLOBALS  # noqa
-from securepy.stdio import IOCage, MemoryOverflow, read_process_output, LimitedStringIO  # noqa
+from securepy.stdio import IOCage, MemoryOverflow, LimitedStringIO  # noqa
 from securepy.timing import IOTimedFunction, TimedFunction, TimedFunctionError  # noqa
 
 __title__ = _conf.NAME
